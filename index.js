ca13c61f5a07e9aaa583dc35b19b1abdcb18610b
PC\Wojtek <W.P.Marczuk@student.tudelft.nl>
2021-02-05 21:19:24 +0100

    help / commands

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+const { prefix } = require('./config.json');
 const command = require('./command.js');
 
 client.on('ready', () => {
     console.log('The client is ready');
 
+    command(client, ['help', 'commands'], message => {
+        const embed = new Discord.MessageEmbed()
+            .setTitle('Help')
+            .setDescription('Below is a list of commands. When called, the bot replies with info on that topic')
+            .addField(`${prefix}ping`, 'Pong!')
+
+        message.channel.send(embed);
+    });
+
     command(client, 'ping', message => {
         message.channel.send('Pong!');
-    })
-})
+    });
+});
 
 client.login(process.env.TOKEN);
