237835d9f2dfe2f786d5e7f9abae4f51bf7cf35a
PC\Wojtek <W.P.Marczuk@student.tudelft.nl>
2021-02-05 19:14:14 +0100

    ping command

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const Discord = require('discord.js')
-const client = new Discord.Client()
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+const command = require('./command.js');
 
 client.on('ready', () => {
-    console.log('The client is ready')
+    console.log('The client is ready');
+
+    command(client, 'ping', message => {
+        message.channel.send('Pong!');
+    })
 })
 
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
