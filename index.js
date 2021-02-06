5b6010261f8e89204a7c97ae3b4f7276533d2832
PC\Wojtek <W.P.Marczuk@student.tudelft.nl>
2021-02-06 03:24:29 +0100

    quadratic formula init

--- index.js
+++ index.js
@@ -17,17 +17,29 @@ client.on('ready', () => {
         .then(console.log)
         .catch(console.error);
 
+    // Descriptions of commands
     command(client, ['help', 'commands'], message => {
         const embed = new Discord.MessageEmbed()
             .setTitle('Help')
             .setDescription('Below is a list of commands. When called, the bot replies with info on that topic')
-            .addField(`${prefix}ping`, 'Pong!');
+            .addField(`${prefix}ping`, `Pong!`)
+            .addField(`${prefix}quad / ${prefix}quadratic / ${prefix}quadraticformula`, `Displays the quadratic formula and help on how to use it`);
 
         message.channel.send(embed);
     });
 
+    // Command for testing
     command(client, 'ping', message => {
         message.channel.send('doop');
+    });
+
+    command(client, ['quad', 'quadratic', 'quadraticformula'], message => {
+        const embed = new Discord.MessageEmbed()
+            .setTitle('The quadratic formula')
+            .attachFiles(['../images/quad/1.png', '../images/quad/2.png'])
+            .setImage('attachment://1.png')
+
+        message.channel.send(embed);
     });
 });
 
