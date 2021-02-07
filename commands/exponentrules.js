82b299c759ed2a0d4def8313fa964a2c46d963a7
rydergithub <78632382+rydergithub@users.noreply.github.com>
2021-02-07 17:31:51 +1300

    Update exponentrules.js

--- commands/exponentrules.js
+++ commands/exponentrules.js
@@ -7,7 +7,7 @@ module.exports = (client) => {
         const exponentRules = new Discord.MessageEmbed()
             .setTitle('Exponent Rules')
             .setDescription('Some of the Rules of Exponents or Laws of Exponents are summarized in the following table.')
-            .attachFiles(['images/trig/exponentrules.png'])
+            .attachFiles(['images/exponent/exponentrules.png'])
             .setImage('attachment://exponentrules.png');
 
         message.channel.send(exponentRules)
